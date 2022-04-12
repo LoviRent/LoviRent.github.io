798af1e2dfd891722508b1309767efa610ad24bb
LoviRent <103351967+LoviRent@users.noreply.github.com>
2022-04-12 13:45:02 +0200

    Add files via upload

---
+++ js/copy.js
@@ -0,0 +1,15 @@
+function copydc() {
+    var copyText = "Das Eichhörnchen#7389";
+    navigator.clipboard.writeText(copyText);
+    toasteinblenden();
+}
+function copymatrix() {
+    var copyText = "@baerlkr:matrix.org";
+    navigator.clipboard.writeText(copyText);
+    toasteinblenden();
+}
+function copymail() {
+    var copyText = "mail@lovisrentsch.de";
+    navigator.clipboard.writeText(copyText);
+    toasteinblenden();
+}
